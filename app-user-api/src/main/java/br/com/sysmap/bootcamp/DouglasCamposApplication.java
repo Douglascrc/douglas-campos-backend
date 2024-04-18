6701730e756cf61db7ee9af0babe5e86d7a948ba
Douglascrc <douglasbuscam@gmail.com>
2024-04-18 20:41:18 -0300

    Utilizando o método Post

---
+++ app-user-api/src/main/java/br/com/sysmap/bootcamp/DouglasCamposApplication.java
@@ -0,0 +1,13 @@
+package br.com.sysmap.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DouglasCamposApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DouglasCamposApplication.class, args);
+	}
+
+}
---
+++ app-user-api/src/main/java/br/com/sysmap/bootcamp/domain/entities/Users.java
@@ -0,0 +1,32 @@
+package br.com.sysmap.bootcamp.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder(toBuilder = true)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "USERS")
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id;
+
+    @Column( name = "name" )
+    private String name;
+
+    @Column( name = "email")
+    private String email;
+
+    @Column( name = "password")
+    private String password;
+
+}
