04e9331eedfe34ef04c941c94bdc9714dfe620e2
Douglascrc <douglasbuscam@gmail.com>
2024-06-28 15:53:55 -0300

    refactor: change description

--- app-integration-api/src/main/java/br/com/sysmap/bootcamp/config/SpringDocOpenApiConfig.java
+++ app-integration-api/src/main/java/br/com/sysmap/bootcamp/config/SpringDocOpenApiConfig.java
@@ -16,10 +16,12 @@ public class SpringDocOpenApiConfig {
         return new OpenAPI()
                 .info(new Info()
                         .title("Bootcamp Sysmap")
-                        .description("REST API USERS Bootcamp 2024 Sysmap")
+                        .description("REST API INTEGRATION Bootcamp 2024 Sysmap")
                         .version("v1")
                         .termsOfService("http://swagger.io/terms/")
-                        .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0"))
                         .contact(new Contact().name("Douglas Campos").email("douglascam@email.com")));
     }
 
