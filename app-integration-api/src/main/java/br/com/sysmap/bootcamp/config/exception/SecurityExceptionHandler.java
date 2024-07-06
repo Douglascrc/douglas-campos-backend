d79e72af4b9757d40c2dfc42eaca6a44d196b70b
Douglascrc <douglasbuscam@gmail.com>
2024-07-06 19:00:46 -0300

    feat: add auth exceptions and exceptions management

---
+++ app-integration-api/src/main/java/br/com/sysmap/bootcamp/config/exception/SecurityExceptionHandler.java
@@ -0,0 +1,28 @@
+package br.com.sysmap.bootcamp.config.exception;
+
+import br.com.sysmap.bootcamp.exceptions.customs.StandardError;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import java.io.IOException;
+import java.time.Instant;
+
+@ControllerAdvice
+@AllArgsConstructor
+public class SecurityExceptionHandler implements AuthenticationEntryPoint {
+
+    private ObjectMapper objectMapper;
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.getWriter().print(objectMapper.writeValueAsString(new StandardError(Instant.now(), HttpStatus.UNAUTHORIZED.value(),
+                "User Not Found",authException.getMessage(),request.getRequestURI())));
+    }
+}
