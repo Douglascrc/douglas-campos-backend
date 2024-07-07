ceacc6bb440a288b5b1e72733866d623af853b09
Douglascrc <douglasbuscam@gmail.com>
2024-07-06 23:26:23 -0300

    refactor: convert to entity and add endpoint for remove by id

--- app-integration-api/src/main/java/br/com/sysmap/bootcamp/domain/service/AlbumService.java
+++ app-integration-api/src/main/java/br/com/sysmap/bootcamp/domain/service/AlbumService.java
@@ -1,18 +1,21 @@
-
 package br.com.sysmap.bootcamp.domain.service;
 
 import br.com.sysmap.bootcamp.domain.entities.Album;
-import br.com.sysmap.bootcamp.domain.entities.Users;
 import br.com.sysmap.bootcamp.domain.model.AlbumModel;
 import br.com.sysmap.bootcamp.domain.repository.AlbumRepository;
 import br.com.sysmap.bootcamp.domain.service.integration.SpotifyApi;
-import br.com.sysmap.bootcamp.dto.WalletDto;
+import br.com.sysmap.bootcamp.dto.RequestAlbumDto;
+import br.com.sysmap.bootcamp.dto.WalletOperationDto;
+import br.com.sysmap.bootcamp.exceptions.customs.IllegalArgsRequestException;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.hc.core5.http.ParseException;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,33 +32,43 @@ public class AlbumService {
 
     private final Queue queue;
     private final RabbitTemplate template;
-    private final SpotifyApi spotifyApi;
+    private final SpotifyApi  spotifyApi;
     private final AlbumRepository albumRepository;
     private final UsersService usersService;
 
 
-    public List<AlbumModel> getAlbums(String search) throws IOException, ParseException, SpotifyWebApiException {
-        return this.spotifyApi.getAlbums(search);
+    public Page<AlbumModel> getAlbums(String search,Pageable pg) throws IOException, ParseException, SpotifyWebApiException {
+        List<AlbumModel> albumList =  spotifyApi.getAlbums(search);
+        log.info("Getting albums");
+        return new PageImpl<>(albumList,pg,albumList.size());
     }
 
     @Transactional(propagation = Propagation.REQUIRED)
-    public Album saveAlbum(Album album) {
-
-        album.setUsers(getUser());
-        Album albumSaved = albumRepository.save(album);
-
-        WalletDto walletDto = new WalletDto(albumSaved.getUsers().getEmail(), albumSaved.getValue());
-        this.template.convertAndSend(queue.getName(), walletDto);
-
-        return albumSaved;
+    public Album saveAlbum(RequestAlbumDto albumDto) {
+        Album album = convertToEntity(albumDto);
+        album.setUsers(usersService.getAuthenticatedUser());
+        if(albumRepository.findAlbumByUsersAndIdSpotify(album.getUsers(), album.getIdSpotify()).isPresent()){
+            throw new IllegalArgsRequestException("Album already purchased");
+        }
+        log.info("Saving album: {}", album);
+        album = albumRepository.save(album);
+        WalletOperationDto walletDto = new WalletOperationDto(album.getUsers().getEmail(), album.getValue());
+        template.convertAndSend(queue.getName(), walletDto);
+        log.info("Sending debit from wallet: {}", walletDto);
+        return album;
     }
 
-    private Users getUser() {
-        String username = SecurityContextHolder.getContext().getAuthentication()
-                .getPrincipal().toString();
-        return usersService.findByEmail(username);
+    private Album convertToEntity(RequestAlbumDto albumDto) {
+        return Album.builder().name(albumDto.name()).artistName(albumDto.artistName())
+                .idSpotify(albumDto.idSpotify()).imageUrl(albumDto.imageUrl()).value(albumDto.value()).build();
     }
 
+    public Page<Album> getUserAlbums(Pageable pg) {
+        return albumRepository.findAllByUsers(usersService.getAuthenticatedUser(),pg);
+    }
 
+    public void delete(Long id) {
+        albumRepository.delete(albumRepository.findAlbumByUsersAndId(usersService.getAuthenticatedUser(),id)
+                .orElseThrow(()-> new EntityNotFoundException("Album Not Found")));
+    }
 }
-
--- app-integration-api/src/main/java/br/com/sysmap/bootcamp/web/AlbumController.java
+++ app-integration-api/src/main/java/br/com/sysmap/bootcamp/web/AlbumController.java
@@ -1,40 +1,81 @@
 package br.com.sysmap.bootcamp.web;
 
-
 import br.com.sysmap.bootcamp.domain.entities.Album;
 import br.com.sysmap.bootcamp.domain.model.AlbumModel;
 import br.com.sysmap.bootcamp.domain.service.AlbumService;
+import br.com.sysmap.bootcamp.dto.RequestAlbumDto;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.apache.hc.core5.http.ParseException;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 import se.michaelthelin.spotify.exceptions.SpotifyWebApiException;
-import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.io.IOException;
-import java.util.List;
+import java.net.URI;
 
-@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/albums")
-@Tag(name="Albums", description = "Albums API")
+@CrossOrigin("*")
 public class AlbumController {
 
     private final AlbumService albumService;
 
-    @Operation(summary = "Get all albums")
     @GetMapping("/all")
-    public ResponseEntity<List<AlbumModel>> getAlbums(@RequestParam("search") String search) throws IOException, ParseException, SpotifyWebApiException {
-        return ResponseEntity.ok(this.albumService.getAlbums(search));
+    @Operation(summary = "get all from spotify by text parameter")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "obtained albums sucessful"),
+            @ApiResponse(responseCode = "401",description = "not authenticated user"),
+            @ApiResponse(responseCode = "500",description = "internal failure to get albums")
+    })
+    public ResponseEntity<Page<AlbumModel>> getAlbums(@RequestParam("search") String search, Pageable pg) throws IOException, ParseException, SpotifyWebApiException {
+        return ResponseEntity.ok(this.albumService.getAlbums(search,pg));
     }
 
-    @Operation(summary = "Save album")
+    @GetMapping("/my-collection")
+    @Operation(summary = "get all from my collection")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "obtained my albums successful"),
+            @ApiResponse(responseCode = "401",description = "not authenticated user"),
+            @ApiResponse(responseCode = "500",description = "internal failure to get albums")
+    })
+    public ResponseEntity<Page<Album>> getByUser(Pageable pg){
+        return ResponseEntity.ok(albumService.getUserAlbums(pg));
+    }
+
     @PostMapping("/sale")
-    public ResponseEntity<Album> saveAlbum(@RequestBody Album album) {
-        return ResponseEntity.ok(this.albumService.saveAlbum(album));
+    @Operation(summary = "buy an album")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",description = "album saved successful"),
+            @ApiResponse(responseCode = "401",description = "not authenticated user"),
+            @ApiResponse(responseCode = "422",description = "invalid fields on the request"),
+            @ApiResponse(responseCode = "500",description = "internal failure to save album")
+    })
+    public ResponseEntity<Album> saveAlbum(@Valid @ParameterObject @RequestBody RequestAlbumDto album, HttpServletRequest http) {
+        Album albumSaved = albumService.saveAlbum(album);
+        URI uri = UriComponentsBuilder.fromUriString(http.getRequestURI()).path("/{id}").buildAndExpand(albumSaved.getId()).toUri();
+        return ResponseEntity.created(uri).body(albumSaved);
     }
 
+    @DeleteMapping("/remove/{id}")
+    @Operation(summary = "remove an album by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204",description = "no content"),
+            @ApiResponse(responseCode = "404",description = "album not found"),
+            @ApiResponse(responseCode = "401",description = "not authenticated user"),
+            @ApiResponse(responseCode = "500",description = "internal failure to get albums")
+    })
+    public ResponseEntity<Void> removeAlbum(@PathVariable Long id) {
+        albumService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
-
