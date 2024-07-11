d0c6b9ab1093893f3a5ce7d31f7c5543c90fa10f
Douglascrc <douglasbuscam@gmail.com>
2024-07-10 21:01:39 -0300

    test: Spotify Api Test

--- app-integration-api/src/main/java/br/com/sysmap/bootcamp/domain/exceptions/AlbumAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package br.com.sysmap.bootcamp.domain.exceptions;
-
-public class AlbumAlreadyExistsException extends RuntimeException {
-    public AlbumAlreadyExistsException(String message) {
-        super(message);
-    }
-}
