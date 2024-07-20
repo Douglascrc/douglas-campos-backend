4cf3cf9bc8091ee21212f082963bc328950e1bd7
Douglascrc <douglasbuscam@gmail.com>
2024-07-20 19:21:08 -0300

    testr: Spotify Api Test

---
+++ app-integration-api/src/test/java/br/com/sysmap/bootcamp/domain/service/integration/SpotifyApiTest.java
@@ -0,0 +1,44 @@
+package br.com.sysmap.bootcamp.domain.service.integration;
+
+import br.com.sysmap.bootcamp.domain.mapper.AlbumMapper;
+import br.com.sysmap.bootcamp.domain.service.integration.SpotifyApi;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import se.michaelthelin.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
+
+@ExtendWith(SpringExtension.class)
+public class SpotifyApiTest {
+
+    @InjectMocks
+    private SpotifyApi spotifyApiService;
+    @Mock
+    Environment env;
+
+    @Mock
+    AlbumMapper albumMapper;
+
+    @Mock
+    private ClientCredentialsRequest clientCredentialsRequest;
+    @Mock
+    private ClientCredentialsRequest.Builder clientCredentialsRequestBuilder;
+
+   /* public void getAlbumsShouldReturnAlbums() throws IOException, ParseException, SpotifyWebApiException {
+        String search = "teste";
+        String token = "example";
+        doNothing().when(spotifyApi).setAccessToken(anyString());
+        when(albumMapper.toModel((any(AlbumSimplified[].class)))).thenReturn(List.of(new AlbumModel()));
+
+        ClientCredentialsRequest.Builder clientCredentialsRequestBuilder = new ClientCredentialsRequest.Builder("cara","teste");
+        when(spotifyApi.clientCredentials()).thenReturn(clientCredentialsRequestBuilder);
+
+        assertDoesNotThrow(()->spotifyApiService.getAlbums(search));
+    }*/
+
+    /*@Test
+    public void testGetToken() throws IOException, ParseException, SpotifyWebApiException {
+        when(clientCredentialsRequest.execute()).thenReturn();
+    }*/
+}
