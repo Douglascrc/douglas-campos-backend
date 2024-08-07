1842d89b4fa062b455a70922ee7ca63e11b822f9
Douglascrc <douglasbuscam@gmail.com>
2024-08-06 21:25:39 -0300

    test: contextLoad

--- app-user-api/src/test/java/br/com/sysmap/bootcamp/DouglasCamposApplicationTests.java
+++ app-user-api/src/test/java/br/com/sysmap/bootcamp/DouglasCamposApplicationTests.java
@@ -2,8 +2,13 @@ package br.com.sysmap.bootcamp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class DouglasCamposApplicationTests {
 
+        @Test
+        void contextLoads() {
+        }
 }
